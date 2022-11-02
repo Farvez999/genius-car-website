88120cd978b3e52f4b7b4b78f8f0015727429623
Farvez Abdullah <engrfrzabdullah999@gmail.com>
2022-11-02 11:31:17 +0600

    Banner Added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { router } from './routers/routers';
 
 function App() {
   return (
-    <div>
+    <div data-theme="cupcake" className='max max-w-screen-lg mx-auto'>
       <RouterProvider router={router} />
     </div>
   );
