90cb13b12634ec92cc6dd82af5033eb14445758b
Farvez Abdullah <engrfrzabdullah999@gmail.com>
2022-11-02 12:23:17 +0600

    Home pahe About section  Added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { router } from './routers/routers';
 
 function App() {
   return (
-    <div data-theme="cupcake" className='max max-w-screen-lg mx-auto'>
+    <div className='max max-w-screen-lg mx-auto'>
       <RouterProvider router={router} />
     </div>
   );
