61606b08db8d7ff15a66abf0b0df48b7bb765ee2
Farvez999 <engrfrzabdullah999@gmail.com>
2022-11-05 19:08:29 +0600

    Get jwt token on client side

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -21,8 +21,31 @@ const Login = () => {
         login(email, password)
             .then(result => {
                 const user = result.user;
-                console.log(user);
-                navigate(from, { replace: true });
+
+
+                const currentUser = {
+                    email: user.email,
+                }
+                console.log(currentUser);
+
+                //JWT Token
+                fetch('http://localhost:5000/jwt',
+                    {
+                        method: 'POST',
+                        headers: {
+                            'content-type': 'application/json'
+                        },
+                        body: JSON.stringify(currentUser)
+                    })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data)
+                        //local storage token set
+                        localStorage.setItem('token', data.token);
+                    })
+
+
+                // navigate(from, { replace: true });
             })
             .then(error => console.log(error));
     }
