924af212965322f8928877f78eb3cf42213588c8
Farvez999 <engrfrzabdullah999@gmail.com>
2022-11-11 21:11:57 +0600

    Genius car find products between price range 100 to 300

--- src/pages/Home/Services/Services.js
+++ src/pages/Home/Services/Services.js
@@ -3,12 +3,13 @@ import ServiceCard from './ServiceCard';
 
 const Services = () => {
     const [services, setServices] = useState([]);
+    const [isAsc, setIsASc] = useState(true);
 
     useEffect(() => {
-        fetch('https://genius-car-server-ten-mocha.vercel.app/services')
+        fetch(`http://localhost:5000/services?order=${isAsc ? 'asc' : 'desc'}`)
             .then(res => res.json())
             .then(data => setServices(data))
-    }, []);
+    }, [isAsc]);
 
     return (
         <div>
@@ -16,6 +17,7 @@ const Services = () => {
                 <p className="text-2xl font-bold text-orange-600">Services</p>
                 <h2 className="text-5xl font-semibold">Our Service Area</h2>
                 <p>the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
+                <button className='btn btn-outline' onClick={() => setIsASc(!isAsc)}>{isAsc ? 'desc' : 'asc'}</button>
             </div>
             <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
                 {
